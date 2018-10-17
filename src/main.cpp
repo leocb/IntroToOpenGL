64ac0cd5fd422b9c49169fd34c328cd158667854
leocb <leobottaro@gmail.com>
2018-10-17 20:45:25 -0300

    #2 - OpenGL Window

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <GL/glew.h>
+#include "display.h"
 
-main(int argc, char const *argv[])
+int main(int argc, char const *argv[])
 {
-  std::cout << "Hello world!" << std::endl;
+  Display display(800, 600, "ITOG Awesome Window");
+
+  while (!display.isClosed())
+  {
+    display.clear(0.0f, 0.15f, 0.3f, 1.0f);
+    display.update();
+  }
+
   return 0;
-}
+};
